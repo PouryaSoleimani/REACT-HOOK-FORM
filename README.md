f9cf03a38c6ca8104f72d077d72114514f8fdc46
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-04-22 04:19:55 -0700

    feat: add particles BG

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## A Mini Login Form Project , Powered By REACT-HOOK-FORM 💜💜💜
 
-![](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)
+![](https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white)
 ![](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)
 ![](https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white)
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,118 @@
 // ^ APP.TSX
-function App() {
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { type Container, type ISourceOptions, MoveDirection, OutMode, } from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
 
+const App = () => {
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#121212",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 220,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.1,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "outside",
+          enable: true,
+          outModes: {
+            default: "out",
+          },
+          random: false,
+          speed: 3,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 10, max: 20 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
 
   return (
     <>
-    <h1>HELLO WORLD</h1>
-    </>
-  )
-}
+      <Particles id="tsparticles" particlesLoaded={particlesLoaded} options={options} className="z-0" />
+      <div className="max-w-md z-50 mx-auto relative overflow-hidden  mt-32 bg-gray-800 p-8 rounded-lg shadow-md before:w-24 before:h-24 before:absolute before:bg-purple-600 before:rounded-full before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-sky-400 after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12">
+        <h2 className="text-2xl font-bold text-white mb-6">LOGIN</h2>
+
+        <form>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-300" htmlFor="name" >Full Name</label>
+            <input className="mt-1 p-2 w-full bg-gray-700 border border-gray-600 rounded-md text-white" type="text" />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-300" htmlFor="email">Email Address</label >
+            <input className="mt-1 p-2 w-full bg-gray-700 border border-gray-600 rounded-md text-white" name="email" id="email" type="email" />
+          </div>
+          <div className="flex justify-center">
+            <button className="bg-gradient-to-r from-purple-600 via-purple-400 to-blue-500 text-white px-16 py-4 font-bold rounded-md hover:opacity-80" type="submit">SUBMIT</button>
+          </div>
+        </form>
+      </div>
+
+
+    </>);
+};
 
 export default App

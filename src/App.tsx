afdcf960f68687a5b0faabe237af5df6df05d2e1
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-04-22 22:34:16 -0700

    feat:particles.js

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { type Container, type ISourceOptions } from "@tsparticles/engine";
 import { loadSlim } from "@tsparticles/slim";
 import { useForm } from 'react-hook-form';
-
+import { DevTool } from '@hookform/devtools';
 
 const App = () => {
   const [init, setInit] = useState(false);
-  const { register, handleSubmit, formState: { errors }, } = useForm({
+  
+  const { register, handleSubmit, control, formState: { errors }, } = useForm({
+    mode: "onChange",
     defaultValues: {
       email: "",
       password: ""
@@ -105,25 +107,25 @@ const App = () => {
 
       <div className="max-w-md z-50 mx-auto relative overflow-hidden  mt-32 bg-gray-800 p-8 rounded-lg shadow-md before:w-24 before:h-24 before:absolute before:bg-purple-600 before:rounded-full before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-sky-400 after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12">
         <h2 className="text-2xl font-bold text-white mb-6">LOGIN</h2>
-
         <form onSubmit={handleSubmit((data) => console.log(data))} >
           <div className="mb-4">
-            <input className="mt-1 p-2 w-full bg-gray-700 border border-gray-600 rounded-md text-white" type="email" placeholder="E-MAIL" {...register("email", { required: true })} />
+            <input className="mt-1 p-2 w-full bg-gray-700 border border-gray-600 rounded-md text-white" type="email" placeholder="E-MAIL" {...register("email", { pattern: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/ })} />
           </div>
-          {errors.email && <p className="text-red-700 font-bold">Email is Required</p>}
+          {errors.email && <p className="text-red-700 font-bold">Email is Invalid</p>}
           <div className="mb-4">
             <input className="mt-1 p-2 w-full bg-gray-700 border border-gray-600 rounded-md text-white" type="password" placeholder="PASSWORD" {...register('password', { required: true })} />
           </div>
-          {errors.password && <p className="text-red-700 font-bold">Password is Required</p>}
+          {errors.password && <p className="text-red-700 font-bold">Invalid Email</p>}
           <div className="flex justify-center">
             <button className="bg-gradient-to-r from-purple-600 via-purple-400 to-blue-500 text-white px-16 py-4 font-bold rounded-md hover:opacity-80" type="submit">SUBMIT</button>
           </div>
 
         </form>
       </div >
+          <DevTool control={control} />
 
 
-    </>);
+    </>); 
 };
 
 export default App

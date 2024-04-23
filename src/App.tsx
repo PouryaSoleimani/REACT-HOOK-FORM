3ef43cd9b03ef87f6868fb734ab96d8b57ff7f5f
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-04-23 01:13:18 -0700

    Form Validation

--- src/App.tsx
+++ src/App.tsx
@@ -95,18 +95,17 @@ const App = () => {
     [],
   );
 
-  //? USEFORM FORM REACT-HOOK-FORM
+  //1-USEFORM FORM REACT-HOOK-FORM
   const { register, handleSubmit, control, formState: { errors } } = useForm<Inputs>({ mode: "onChange", defaultValues: { email: "", password: "" } });
 
-  //^ SUBMIT HANDLER
-  const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => { console.log(data); setData(data); }
+  //2-SUBMIT HANDLER
+  const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => { console.log(data); setData(data) }
 
 
-  // & RETURN =============================================================================================================================
+  //^ RETURN =============================================================================================================================
   return (
     <>
       {/* <Particles id="tsparticles" particlesLoaded={particlesLoaded} options={options} className="z-0" /> */}
-
       <div className="max-w-md z-50 mx-auto relative overflow-hidden  mt-32 bg-gray-800 p-8 rounded-lg shadow-md before:w-24 before:h-24 before:absolute before:bg-purple-600 before:rounded-full before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-sky-400 after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12">
         <h2 className="text-2xl font-bold text-white mb-6">LOGIN</h2>
         <form onSubmit={handleSubmit(onSubmit)} >
